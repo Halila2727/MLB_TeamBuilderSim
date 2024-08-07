4a0ade24cee53843ea49a83f32034c37fbf580fd
Halil Akca <halil.akca45@myhunter.cuny.edu>
2024-08-07 16:47:38 -0400

    figured out how to populate teams with imported players

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main 
 {
+    private static final Map<String, String> fantasyTeam = new HashMap<>();
+
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
         String input = "";
 
-        System.out.println("Welcome to the Baseball Team Selector!");
-        System.out.println("This program will randomly select a team for you.");
-
-        System.out.print("Do you want to pick a randomly selected team? (Type Y to continue): ");
-        System.out.flush();  // Ensure the prompt is displayed immediately
-
         while (true) 
         {
+            System.out.print("Pick a randomly selected team? (Type Y if yes): ");
             input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("Y")) {
-                TeamSelector selectedTeam = new TeamSelector();
-                System.out.println("Your randomly selected team is: " + selectedTeam.getTeam());
-                System.out.println(); // Add a blank line for better readability
-                break;
-            } 
-            else 
+            if (input.equalsIgnoreCase("Y")) 
             {
-                System.out.println("Invalid input. Please type Y for yes");
-                System.out.flush();  // Ensure the prompt is displayed immediately
-            }
+                String selectedTeam = Team.selectRandomTeam();
+                System.out.println("Your team is " + selectedTeam);
+
+                List<Player> players = Team.getPlayersForTeam(selectedTeam);     
+            } 
+            else { break; }
         }
 
         scanner.close();
